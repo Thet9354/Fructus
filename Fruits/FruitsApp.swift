c8ec829ff5e91aea255c0fbae278b0da2cfcee8c
PhoonThetPine <thetpine254@gmail.com>
2023-10-09 10:06:08 +0800

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Phoon Thet Pine on 9/10/23.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
